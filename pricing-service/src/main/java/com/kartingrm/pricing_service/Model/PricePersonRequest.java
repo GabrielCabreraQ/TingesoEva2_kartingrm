81a4c6fd53428138dfc378d58dc9a6a6ecc25366
Gabriel Cabrera <gabriel.cabrera.q@usach.cl>
2025-06-05 18:06:05 -0400

    config

--- pricing-service/src/main/java/com/kartingrm/pricing_service/Model/PricePersonRequest.java
+++ pricing-service/src/main/java/com/kartingrm/pricing_service/Model/PricePersonRequest.java
@@ -1,6 +1,6 @@
 package com.kartingrm.pricing_service.Model;
 
-import com.kartingrm.Pricing_service.Entity.Pricing;
+import com.kartingrm.pricing_service.Entity.Pricing;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
