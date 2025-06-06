21e1e4031f512ffe0cd10e6296ce89680fee7f98
Gabriel Cabrera <gabriel.cabrera.q@usach.cl>
2025-06-05 23:39:42 -0400

    -cross *

--- specialdays-service/src/main/java/com/kartingrm/specialdays_service/Controller/SpecialDaysController.java
+++ specialdays-service/src/main/java/com/kartingrm/specialdays_service/Controller/SpecialDaysController.java
@@ -14,7 +14,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/specialdays")
-@CrossOrigin("*")
 public class SpecialDaysController {
     @Autowired
     private SpecialDaysService specialDaysService;
